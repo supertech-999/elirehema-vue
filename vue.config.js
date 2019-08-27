091b19292e30c9688bb18bbbdd0c799db16ee403
unitech1120 <uni.tech1120@gmail.com>
2019-08-27 16:10:00 +0300

    reconfig deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 // vue.config.js
 module.exports = {
+<<<<<<< Updated upstream
   publicPath: process.env.NODE_ENV === 'production'
     ? '/Onn/'
     : '/'
+=======
+  
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Onn/'
+    : '/'
+
+>>>>>>> Stashed changes
 }
